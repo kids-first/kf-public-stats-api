5bacc3350e1d5e043d34c7379255e7710a2cada2
Celine Pelletier <cpelletier@ferlab.bio>
2021-11-11 16:20:13 -0500

    SKFP-296: Update kf-public-stats-api to use keycloak instead of ego for authentication

---
+++ src/services/keycloak.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosBasicCredentials } from 'axios';
+import * as jwt from 'jsonwebtoken';
+import * as qs from 'query-string';
+
+import { keycloakClientId, keycloakClientSecret } from '../config';
+
+let appToken: string;
+let expiry: number;
+
+const setToken = (token) => {
+  appToken = token;
+  expiry = jwt.decode(appToken).exp * 1000;
+};
+
+const fetchToken = async (): Promise<string> => {
+  const data = qs.stringify({
+    'grant_type': 'client_credentials',
+    'scope': 'openid' 
+  });
+  const config = {
+    method: 'post',
+    url: 'https://kf-keycloak-qa.kf-strides.org/auth/realms/kidsfirstdrc/protocol/openid-connect/token',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      username: keycloakClientId,
+      password: keycloakClientSecret
+    } as AxiosBasicCredentials,
+    data : data
+  };
+  
+  try{
+    const response = await axios(config);
+
+    return response.data.access_token;
+  } catch(err){
+    throw new Error(`Error authenticating with keycloak: ${err.message}`);
+  }
+};
+
+const isTokenExpired = (): boolean => {
+  return !expiry || Date.now() > expiry;
+};
+
+const getToken = async (): Promise<string> => {
+  if (!appToken || isTokenExpired()) {
+    setToken(await fetchToken());
+  }
+
+  return appToken;
+};
+
+export default { getToken };
--- src/services/searchMembers.ts
+++ src/services/searchMembers.ts
@@ -1,12 +1,12 @@
 import {searchMembersApi} from '../config';
-import ego from './ego';
+import keycloak from './keycloak';
 
 import axios from 'axios';
 
 const getInterestCounts = async (): Promise<PropertyCounts> => {
   const url = `${searchMembersApi}/interests_stats`;
 
-  const token = await ego.getToken();
+  const token = await keycloak.getToken();
   const Authorization: string = `Bearer ${token}`;
   const config = {
     headers: { Authorization, 'Content-Type': 'application/json' },
